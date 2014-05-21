f976ac6bcc1dbfb56e40bb81ae82a548020d66ec
vkorlakunta <korlakuntasamba@gmail.com>
2014-05-21 12:14:21 -0400

    Initial Commit
    Initial commit for rest full web services Project..

---
+++ src/com/src/rest/Status.java
@@ -0,0 +1,27 @@
+package com.src.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/status/")
+public class Status {
+	
+	public  static final String api_version = "00.1.0";
+	
+	@Path("/status")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getStatus() {
+		return "</p> Hello... Restful Webservices..!!! </p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getVersion() {
+		return "</p> Version is :: </p>" + api_version;
+	}
+
+}
